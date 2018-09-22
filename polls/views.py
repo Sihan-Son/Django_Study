797d15fd408553054284a0fe7572ebbc596604a2
Sihan-Son <sihan830@gmail.com>
2018-09-22 16:10:22 +0900

    [UPDATE] define detail

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from polls.models import Question
 
 
@@ -7,3 +7,7 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
